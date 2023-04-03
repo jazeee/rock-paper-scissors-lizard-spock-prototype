0536455a2e7352fe9a9bd76a94e8888e04b6872b
Jaz Singh <jazeee@gmail.com>
2023-04-03 16:06:30 -0700

    Update eslint and tsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,91 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        groups: [['builtin', 'external'], 'internal', ['parent', 'sibling'], 'index', 'object'],
+        pathGroups: [
+          {
+            pattern: '@*{,/**}',
+            group: 'external',
+          },
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    curly: ['error', 'all'],
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      {
+        json: 'always',
+        tsx: 'never',
+        ts: 'never',
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'no-console': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/index.ts*'],
+      rules: {
+        'no-restricted-exports': 'off',
+      },
+    },
+    {
+      files: ['**/*test.ts*', 'src/setupTests.ts'],
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
+};
